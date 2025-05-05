4cc0d9d1d95ef9298bc7d8ee9257b427b5b0f017
Franklin <franklinvelsquezhorna@gmail.com>
2025-05-05 00:27:48 -0500

    Proyecto de Grupo

---
+++ ec2.tf
@@ -0,0 +1,21 @@
+##resource
+resource "aws_instance" "nginx_server" {
+  ami                    = "ami-058a8a5ab36292159"  # Amazon Linux 2 (ajusta según la región)
+  instance_type          = var.instance_type
+  key_name               = aws_key_pair.hotel_project.key_name  # apunta al recurso en key.tf
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  tags = {
+    Name = "HotelNginxServer"
+  }
+#que inicializa el servicio 
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              amazon-linux-extras install nginx1 -y
+              systemctl start nginx
+              systemctl enable nginx
+              EOF
+
+  
+}
---
+++ terraform.tfvars
@@ -0,0 +1,7 @@
+bucket_name = "hotel-reservas-web"
+db_username = "admin"
+db_password = "*Zyst3was*"
+db_name     = "hotel_reservas"
+key_name    = "nginx-server.key"
+ses_email   = "franklin131@hotmail.com"
+vpc_id = "vpc-016efe07a8fa1c51d"
