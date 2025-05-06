cf74753049116f9204c7ebef3e9a9484befe988d
Franklin <franklinvelsquezhorna@gmail.com>
2025-05-05 19:05:50 -0500

    Actualización del README

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,11 @@
 ##resource
+# Lanza una instancia EC2 que servirá como servidor web (Nginx)
+# - Utiliza Amazon Linux 2
+# - Usa el Security Group 'web_sg' para permitir acceso externo
+# - Asocia la clave SSH 'hotel_project'
+# - Inicializa Nginx automáticamente con user_data
 resource "aws_instance" "nginx_server" {
-  ami                    = "ami-058a8a5ab36292159"  # Amazon Linux 2 (ajusta según la región)
+  ami                    = "ami-058a8a5ab36292159"  # Amazon Linux 2 AMI (HVM3
   instance_type          = var.instance_type
   key_name               = aws_key_pair.hotel_project.key_name  # apunta al recurso en key.tf
   vpc_security_group_ids = [aws_security_group.web_sg.id]
